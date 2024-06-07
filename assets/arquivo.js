f82b39c190f17e0d0842fcb2908661c3df7fd669
cristielle <cristielle.santos94@gmail.com>
2024-06-07 10:48:50 -0300

    feat: overlay ao abrir o menu responsivo

--- assets/arquivo.js
+++ assets/arquivo.js
@@ -1,14 +1,22 @@
-function menuShow(){
+
+function menuShow() {
     let menuMobile = document.querySelector('.menu-mobile');
+    let overlay = document.querySelector('.overlay');
+
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
+        overlay.classList.add('show');
+        
         document.querySelector('.icon').classList.add('fa-x');
         document.querySelector('.icon').classList.remove('fa-bars');
 
-    } else{
+    } else {
         menuMobile.classList.add('open');
+        overlay.classList.remove('show');
+
+
         document.querySelector('.icon').classList.add('fa-bars');
         document.querySelector('.icon').classList.remove('fa-x');
-
     }
 }
+
